e1611af3c88977781a33662c7c1cb4b53f714c03
etkmao <etkmao@tecent.com>
2023-09-21 16:25:52 +0800

    feat(perf): run video page

---
+++ examples/ios-demo/HippyDemo/QLHippyJSBridgeModule.h
@@ -0,0 +1,18 @@
+//
+//  QLHippyJSBridgeModule.h
+//  TencentVideo
+//
+//  Created by krisxu on 2022/6/16.
+//  Copyright © 2022 Tencent Inc. All rights reserved.
+//
+
+#import "HippyBridgeModule.h"
+#import "HippyEventObserverModule.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface QLHippyJSBridgeModule : HippyEventObserverModule <HippyBridgeModule>
+
+@end
+
+NS_ASSUME_NONNULL_END
